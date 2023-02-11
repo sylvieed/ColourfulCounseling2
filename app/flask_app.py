23a262aca6d61f6fbb9eb0d2f5fe0f2156be50ad
sylviedd <syisda@gmail.com>
2023-02-11 03:59:37 +0000

    change file structure

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-from app import app
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+print("HELLO")
 
 @app.route("/")
 def home():
@@ -20,34 +23,34 @@ def new():
 @app.route("/journals/photography", methods=["GET", "POST"])
 def photography():
     if request.method == "POST":
-        pass   
-    
+        pass
+
     return render_template('photography.html')
 
 @app.route("/journals/collage", methods=["GET", "POST"])
 def collage():
     if request.method == "POST":
-        pass   
-    
+        pass
+
     return render_template('collage.html')
 
 @app.route("/journals/draw", methods=["GET", "POST"])
 def draw():
     if request.method == "POST":
-        pass   
-    
+        pass
+
     return render_template('draw.html')
 
 @app.route("/journals/upload")
 def upload():
     if request.method == "POST":
-        pass   
-    
+        pass
+
     return render_template('upload.html')
 
 @app.route("/journals/write")
 def write():
     if request.method == "POST":
-        pass   
-    
+        pass
+
     return render_template('write.html')
