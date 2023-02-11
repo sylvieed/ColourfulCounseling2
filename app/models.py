8bc8863ed715f62f459a4216b4d84c7762555192
sylviedd <syisda@gmail.com>
2023-02-11 15:45:48 +0000

    Add journaling functionality

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Journal(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     time_created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
+    prompt = db.Column(db.String(200))
     entry1 = db.Column(db.Text)
     entry2 = db.Column(db.Text)
     entry3 = db.Column(db.Text)
