6c1a5aa3c4413ddcd86dd7ac2923de523765ed0b
cdp72 <cdp72@tux.cs.drexel.edu>
2023-03-11 16:38:50 -0500

    removed prompts and added chat box with OpenAI

---
+++ app/promptGenarator.py
@@ -0,0 +1,20 @@
+import openai
+
+openai.api_key = "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"
+
+messages = [
+    {"role": "system", "content": "You are a prompt generator that creates prompts for users to make art to for their art therapy art work. Do not answer anything other then when the user asks for a prompt to draw to relating to art therapy drawing. For example it could be Draw a picture of a place that brings you peace and tranquility. Do not reply with anything except for the prompts. Only show one prompt at a time."},
+]
+
+def chatbot(input):
+    if input:
+        messages.append({"role": "user", "content": input})
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+        return reply
+
+if __name__ == "__main__":
+    print(chatbot("Generate a prompt"))
